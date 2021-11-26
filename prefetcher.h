73b38d826626b90ef7be47345055427084b1f3d0
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-26 13:12:15 -0800

    Update prefetcher.h

--- prefetcher.h
+++ prefetcher.h
@@ -1,5 +1,7 @@
-/* 
+/*
+ *
  * File: prefetcher.h
+ * Author: Sat Garcia (sat@cs)
  * Description: Header file for prefetcher implementation
  *
  */
@@ -9,63 +11,46 @@
 
 #include <sys/types.h>
 #include "mem-sim.h"
-#include <cstdlib>
-#include <cstring>
 
-/* For state calculations, the individual descriptions are given in
-   prefetcher.C.  Assuming the internal variables for the functions
-   can be ignored, the state is given by the private variables, and 
-   the tables used.  The private variables are a bool, a Request and 
-   an int, which sum to 5 bools and 4 ints ~ 17 bytes.  The tagged 
-   table size is given by STATE_SIZE = 2048 bytes.  The rpt_check 
-   table uses 128/8 = 16 bytes, and the RPT table itself uses 128
-   * 12 bytes = 1536.  Thus, in total we have:
-   17 + 16 + 1536 + 2048 = 3617 bytes. */
-#define STATE_SIZE 4096  /* STATE calculation: 2kb for tagging */
-#define BITS_PER_CHAR 8
-#define L2_BLOCK_SIZE 32
-#define NUM_REQS_PER_MISS 2
+#define NUM_RPT_ENTRIES 128 
+#define NUM_MAX_REQUESTS 40 
+#define REQUEST_CUTOFF 20
+#define DEFAULT_STREAMSIZE 2
 
-#define NUM_RPT_ENTRIES 128 /*STATE 128 * 12 = 1536 (1792 with tag)*/ 
-#define WORTHWHILE_RPT 128
+struct RPT {
+    u_int32_t pc;
+    u_int32_t prev_addr;
+    u_int32_t stride;
+    int32_t state;
+} ;// reference prediction table.
 
-typedef struct rpt_row_entry{ /* each row has 4 * 3 = 12 bytes */
-  unsigned int pc;
-  unsigned int last_mem;
-  int last_stride; 
-} rpt_row_entry_t;
 
 class Prefetcher {
-  private:
-	bool _ready;
-	Request _nextReq;
-	int _req_left;
-	
+    private:
+        int num_requests;
+        int num_rpt;
+        Request requests[NUM_MAX_REQUESTS];
+        RPT rpt[NUM_RPT_ENTRIES];
+        int current_pending_request;
+        int oldest_rpt;
+    public:
+        Prefetcher();
+        // should return true if a request is ready for this cycle
+        bool hasRequest(u_int32_t cycle);
 
-  public:
-	Prefetcher();
+        // request a desired address be brought in
+        Request getRequest(u_int32_t cycle);
 
-	/* functions for dealing with the array */
-	/*	bool checkPrefetched(u_int32_t addr);
-	void markPrefetched(u_int32_t addr);
-	void markUnPrefetched(u_int32_t addr);
-	bool bitArrayTest(void); */
+        // this function is called whenever the last prefetcher request was successfully sent to the L2
+        void completeRequest(u_int32_t cycle);
 
-
-	// should return true if a request is ready for this cycle
-	bool hasRequest(u_int32_t cycle);
-
-	// request a desired address be brought in
-	Request getRequest(u_int32_t cycle);
-
-	// this function is called whenever the last prefetcher request was successfully sent to the L2
-	void completeRequest(u_int32_t cycle);
-
-	/*
-	 * This function is called whenever the CPU references memory.
-	 * Note that only the addr, pc, load, issuedAt, and HitL1 should be considered valid data
-	 */
-	void cpuRequest(Request req); 
+        /*
+         * This function is called whenever the CPU references memory.
+         * Note that only the addr, pc, load, issuedAt, and HitL1 should be considered valid data
+         */
+        void cpuRequest(Request req);
+        void addRequest(u_int32_t addr);
+        bool ifAlreadyInQueue(u_int32_t addr);
 };
 
 #endif
