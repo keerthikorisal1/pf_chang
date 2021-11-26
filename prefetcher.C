d52cf1d402e5d61dcd4a97074129e353fe93af90
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-25 22:58:08 -0800

    Update prefetcher.C

--- prefetcher.C
+++ prefetcher.C
@@ -1,257 +1,268 @@
 #include "prefetcher.h"
 #include "mem-sim.h"
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
 
-Prefetcher::Prefetcher() {
-  initLocalRequest();
-  initHistoryState();
+using namespace std;
+
+Prefetcher::Prefetcher()
+{
+  _policyBasePriorities.push_back(65);
+  _policyBasePriorities.push_back(31);
+  _policyBasePriorities.push_back(100);
+  _policyBasePriorities.push_back(80);
 }
 
-bool Prefetcher::hasRequest(u_int32_t cycle) {
-  return (!ifEmptyLocalRequest());
+bool Prefetcher::hasRequest(u_int32_t cycle)
+{
+  return !_reqQueue.empty();
 }
 
-Request Prefetcher::getRequest(u_int32_t cycle) {
-  Request req;
-  req.addr = getFrontLocalRequest();
-  return req;
+Request Prefetcher::getRequest(u_int32_t cycle)
+{
+  return _reqQueue.back().first;
 }
 
-void Prefetcher::completeRequest(u_int32_t cycle) {
-  deLocalRequest();
-}
-
-void Prefetcher::cpuRequest(Request req) {
-  u_int16_t index = queryHistoryState(req.pc);
-  if (index == NULL_STATE) {
-    insertHistoryState(req.pc, req.addr, 1, 0, 0);
-    enLocalRequest(req.addr + 32);
-    enLocalRequest(req.addr + 64);
+void Prefetcher::completeRequest(u_int32_t cycle)
+{
+  _recentRequests.push_back(_reqQueue.back());
+  while(_recentRequests.size() > 1)
+    {
+      _recentRequests.erase(_recentRequests.begin());
+    }
+  _reqQueue.pop_back();
+  if(_reqQueue.size() > 10)
+    {
+      _reqQueue.erase(_reqQueue.begin());
+    }
+  while(_reqQueue.size()>500)
+  {
+    _reqQueue.erase(_reqQueue.begin());
   }
-  else {
-    // Second time
-    if (historyState[index].count == 1) {
-      historyState[index].offset = (int16_t)((int64_t)req.addr) - ((int64_t)historyState[index].addr);
-      historyState[index].addr = req.addr;
-      historyState[index].count++;
-      if (req.HitL1) {
-        if (historyState[index].offset > 0) {
-          historyState[index].ahead = (L1_CACHE_BLOCK - (req.addr % L1_CACHE_BLOCK)) / historyState[index].offset;
-        }
-        else if (historyState[index].offset < 0) {
-          historyState[index].ahead = (req.addr % L1_CACHE_BLOCK) / (- historyState[index].offset);
-        }
-        else {
-          historyState[index].ahead = 0;
-        }
+}
+
+vector<TReqPair> Prefetcher::GetOffsetRequests()
+{
+  vector<TReqPair> ret;
+  Request tempReq;
+  ReqPriority tempPriority;
+  if (_offsets.size() > 2)
+    {
+	if (_offsets[_offsets.size()-1]==_offsets[_offsets.size()-2] &&
+	    _offsets[_offsets.size()-2]==_offsets[_offsets.size()-3] &&
+	    _offsets.back() != 0)
+	  {
+	    tempReq.addr=_arrivals.back().addr + _offsets.back();
+	    tempPriority.priority=_policyBasePriorities[0];
+	    tempPriority.policy=0;
+	    ret.push_back(make_pair(tempReq, tempPriority));
+	    tempReq.addr += _offsets.back();
+	    tempPriority.priority=_policyBasePriorities[0];
+	    tempPriority.policy=0;
+	    ret.push_back(make_pair(tempReq, tempPriority));
+	  }
+	else
+	  {
+	    if (_offsets[_offsets.size()-1]==_offsets[_offsets.size()-2] &&
+		_offsets.back() != 0)
+	      {
+		tempReq.addr=_arrivals.back().addr + _offsets.back();
+		tempPriority.priority=_policyBasePriorities[0]+1;
+		tempPriority.policy=0;
+		ret.push_back(make_pair(tempReq, tempPriority));
+	      }
+	  }
       }
-      else {
-        if (historyState[index].offset > 0) {
-          historyState[index].ahead = (L2_CACHE_BLOCK - (req.addr % L2_CACHE_BLOCK)) / historyState[index].offset;
-        }
-        else if (historyState[index].offset < 0) {
-          historyState[index].ahead = (req.addr % L2_CACHE_BLOCK) / (- historyState[index].offset);
-        }
-        else {
-          historyState[index].ahead = 0;
-        }
-      }
+  return ret;
+}
+
+vector<TReqPair> Prefetcher::GetTemporalRequests()
+{
+  ReqPriority tempPriority;
+  Request tempReq;
+  u_int32_t max_count=0;
+  u_int32_t address;
+  for (map<u_int32_t,u_int32_t>::iterator it=_temporalMap.begin(); 
+       it!=_temporalMap.end(); ++it)
+    {
+      if( it->second > max_count )
+	{
+	  max_count=it->second;
+	  address=it->first;
+	}
     }
-    else {
-      int16_t previous = historyState[index].offset;
-      historyState[index].offset = (int64_t)req.addr - (int64_t)historyState[index].addr;
-      historyState[index].addr = req.addr;
-      if (previous != historyState[index].offset) {
-        historyState[index].count = 1;
-        enLocalRequest(req.addr + 32);
-        enLocalRequest(req.addr + 64);
-      }
-      else {
-        historyState[index].count++;
-      }
-      if (historyState[index].offset != 0) {
-        historyState[index].ahead--;
-      }
+  vector<TReqPair> ret;
+  tempPriority.priority=_policyBasePriorities[1];
+  tempPriority.policy=1;
+  tempReq.addr=address;
+  ret.push_back(make_pair(tempReq,tempPriority));
+  return ret;
+}
+
+vector<TReqPair> Prefetcher::GetLookAheadRequests( Request req )
+{
+  vector<TReqPair> ret;
+  Request tempReq;
+  ReqPriority tempPriority;
+  tempReq.addr = req.addr + 64;
+  tempPriority.priority = _policyBasePriorities[2];
+  tempPriority.policy=2;
+  ret.push_back( make_pair(tempReq, tempPriority) );
+
+  tempReq.addr += 16;
+  tempPriority.priority = _policyBasePriorities[2]+1;
+  tempPriority.policy=2;
+  ret.push_back( make_pair(tempReq, tempPriority) );
+
+  tempReq.addr += 16;
+  tempPriority.priority = _policyBasePriorities[2]+1;
+  tempPriority.policy=2;
+  ret.push_back( make_pair(tempReq, tempPriority) );
+
+  return ret;
+}
+
+std::vector<TReqPair> Prefetcher::GetPeriodicRequests()
+{
+  vector<TReqPair> ret;
+  Request tempReq;
+  ReqPriority tempPriority;
+  bool redundant=0;
+  for(unsigned int i=0; i<_periodicRequests.size(); ++i)
+    {
+      if(_periodicRequests[i]==_arrivals[_arrivals.size()-1].addr)
+	{
+	  redundant=1;
+	  break;
+	}
+    }
+  if(!redundant)
+    {
+      _periodicRequests.clear();
+      if(_arrivals.size() > 3)
+	{
+	  for(unsigned int i=_arrivals.size()-3; i>floor(_arrivals.size()/2.0); --i)
+	    {
+	      if(_arrivals[i].addr == _arrivals[_arrivals.size()-1].addr &&
+		 _arrivals[i].addr == _arrivals[2*i+1-_arrivals.size()].addr)
+		{
+		  for(unsigned int j=i+2; j<_arrivals.size()-1; j++)
+		    {
+		      tempReq.addr = _arrivals[j].addr;
+		      tempPriority.priority = _policyBasePriorities[3]+_arrivals.size()-2-j;
+		      tempPriority.policy=3;
+		      ret.push_back( make_pair(tempReq, tempPriority) );
+		      _periodicRequests.push_back(_arrivals[j].addr);
+		    }
+		  break;
+		}
+	    }
+	}
+    }
+  return ret;
+}
+
+
+void Prefetcher::insertRequest( TReqPair newReq )
+{
+  if( _reqQueue.empty( ) )
+    {
+      _reqQueue.push_back(newReq);
+      return;
+    }
+  bool found=0;
+  for( size_t j=0; j<_reqQueue.size( ); ++j )
+    {
+      if( _reqQueue[j].first.addr==newReq.first.addr )
+	{
+	  _reqQueue[j].second.priority=floor((_reqQueue[j].second.priority+newReq.second.priority)/2.0);
+	  _reqQueue[j].second.policy=newReq.second.policy;
+	  sort(_reqQueue.begin(), _reqQueue.end(), ReqComp());
+	  found=1;
+	  break;
+	}
+    }
+  if (!found)
+    {
+      for (vector<TReqPair>::iterator it = _reqQueue.end()-1;;--it)
+	{
+	  if (newReq.second.priority < it->second.priority)
+	    {
+	      ++it;
+	      _reqQueue.insert(it, newReq);
+	      break;
+	    }
+	  if (_reqQueue.begin() == it)
+	    {
+	      _reqQueue.insert(it, newReq);
+	      break;
+	    }
+	}
+    }
+}
+
+void Prefetcher::cpuRequest(Request req)
+{
+  if(req.HitL1)
+    {
+      for(unsigned int i=0; i<_recentRequests.size(); ++i)
+	{
+	  if(req.addr==_recentRequests[i].first.addr)
+	    {
+	      if(_policyBasePriorities[_recentRequests[i].second.policy]>2)
+		{
+		  _policyBasePriorities[_recentRequests[i].second.policy]-=2;
+		}
+	    }
+	}
+    }
+  if (!_arrivals.empty())
+    {
+	if(_offsets.size() > 2 )
+	  {
+	    _offsets.erase(_offsets.begin());
+	  }
+	_offsets.push_back((req.addr & 0x1EE) - (_arrivals.back().addr & 0x1EE));
+    }
+  if (_arrivals.size( ) > 20 )
+    {
+      _arrivals.erase(_arrivals.begin());
+    }
+  if (_temporalMap.size() > 50 )
+    {
+      _temporalMap.clear();
+    }
+  _arrivals.push_back(req);
+
+  if(!req.HitL1)
+    {
+      ++_temporalMap.insert(make_pair(req.addr,0)).first->second;
     }
 
-    // Prefetch MIN(count - 1, PREFETCH_DEGREE)
-    if (historyState[index].offset != 0) {
-      u_int16_t prefetchDegree = (req.HitL1) ? (L1_PREFETCH_DEGREE) : (L2_PREFETCH_DEGREE);
-      while (historyState[index].ahead < MIN(historyState[index].count - 1, prefetchDegree)) {
-        u_int32_t addr = (int64_t)historyState[index].addr + (int64_t)(historyState[index].offset) *
-                         ((int64_t)historyState[index].ahead + 1);
-        enLocalRequest(addr);
-        if (req.HitL1) {
-          if (historyState[index].offset > 0) {
-            historyState[index].ahead += (L1_CACHE_BLOCK - (addr % L1_CACHE_BLOCK)) / historyState[index].offset + 1;
-          }
-          else {
-            historyState[index].ahead += (addr % L1_CACHE_BLOCK) / (- historyState[index].offset) + 1;
-          }
-        }
-        else {
-          if (historyState[index].offset > 0) {
-            historyState[index].ahead += (L2_CACHE_BLOCK - (addr % L2_CACHE_BLOCK)) / historyState[index].offset + 1;
-          }
-          else {
-            historyState[index].ahead += (addr % L1_CACHE_BLOCK) / (- historyState[index].offset) + 1;
-          }
-        }
-      }
+  vector<TReqPair> requests = GetLookAheadRequests(req);	
+  for( size_t i=0; i<requests.size( ); ++i )
+    {
+      insertRequest( requests[i] );
     }
-
-    //Place to the most recent used head
-    if (historyState[index].next == NULL_STATE) {
-      u_int16_t prev = getSecondLRUState();
-
-      historyState[prev].next = NULL_STATE;
-      historyState[index].next = stateHead;
-      stateHead = index;
+  requests = GetOffsetRequests();
+  for( size_t i=0; i<requests.size( ); ++i )
+    {
+      insertRequest( requests[i] );
     }
-    else {
-      u_int16_t next = historyState[index].next;
-
-      State temp;
-      temp.pc     = historyState[index].pc;
-      temp.addr   = historyState[index].addr;
-      temp.count  = historyState[index].count;
-      temp.offset = historyState[index].offset;
-      temp.ahead  = historyState[index].ahead;
-
-      historyState[index].pc      = historyState[next].pc;
-      historyState[index].addr    = historyState[next].addr;
-      historyState[index].count   = historyState[next].count;
-      historyState[index].offset  = historyState[next].offset;
-      historyState[index].ahead   = historyState[next].ahead;
-      historyState[index].next    = historyState[next].next;
-
-      historyState[next].pc     = temp.pc;
-      historyState[next].addr   = temp.addr;
-      historyState[next].count  = temp.count;
-      historyState[next].offset = temp.offset;
-      historyState[next].ahead  = temp.ahead;
-      historyState[next].next   = stateHead;
-      stateHead = next;
+	
+  if( _temporalMap.size()%50 == 0 )
+    {
+      requests = GetTemporalRequests();
+      for( size_t i=0; i<requests.size( ); ++i )
+	{
+	  insertRequest( requests[i] );
+	}
     }
-  }
-}
-
-// History state table operations
-void Prefetcher::initHistoryState() {
-  stateCount = 0;
-  stateHead = NULL_STATE;
-}
-
-bool Prefetcher::ifEmptyHistoryState() {
-  return (stateCount == 0);
-}
-
-bool Prefetcher::ifFullHistoryState() {
-  return (stateCount == MAX_STATE_COUNT);
-}
-
-void Prefetcher::insertHistoryState(u_int32_t pc, u_int32_t addr, u_int16_t count, int16_t offset, u_int16_t ahead) {
-  if (!ifFullHistoryState()) {
-    // Insert state when table is not full
-    historyState[stateCount].pc     = pc;
-    historyState[stateCount].addr   = addr;
-    historyState[stateCount].count  = count;
-    historyState[stateCount].offset = offset;
-    historyState[stateCount].ahead  = ahead;
-    historyState[stateCount].next   = stateHead;
-    
-    stateHead = stateCount;
-    stateCount++;
-  }
-  else {
-    // LRU eviction in history state table
-    u_int32_t index = getSecondLRUState();
-    u_int32_t replace = historyState[index].next;
-    historyState[index].next = NULL_STATE;
-
-    historyState[replace].pc      = pc;
-    historyState[replace].addr    = addr;
-    historyState[replace].count   = count;
-    historyState[replace].offset  = offset;
-    historyState[replace].ahead   = ahead;
-    historyState[replace].next    = stateHead;
-
-    stateHead = replace;
-  }
-}
-
-u_int16_t Prefetcher::queryHistoryState(u_int32_t pc) {
-  u_int16_t i;
-  for (i = stateHead; i != NULL_STATE; i = historyState[i].next ) {
-    if (historyState[i].pc == pc) {
-      return i;
+  requests = GetPeriodicRequests();
+  for( size_t i=0; i<requests.size( ); ++i )
+    {
+      insertRequest( requests[i] );
     }
-  }
-  return NULL_STATE;
-}
-
-u_int16_t Prefetcher::getSecondLRUState() {
-  u_int16_t i;
-  for (i = 0; i < MAX_STATE_COUNT; i++) {
-    if (historyState[i].next == NULL_STATE) {
-      continue;
-    }
-    else if (historyState[historyState[i].next].next == NULL_STATE) {
-      return i;
-    }
-  }
-  printf("Error -- from Prefetcher::getSecondLRUState()");
-}
-
-// Local request queue operations
-void Prefetcher::initLocalRequest() {
-  frontRequest = rearRequest = 0;
-}
-
-bool Prefetcher::ifEmptyLocalRequest() {
-  return (frontRequest == rearRequest);
-}
-
-bool Prefetcher::ifFullLocalRequest() {
-  return (frontRequest == (rearRequest + 1) % MAX_REQUEST_COUNT);
-}
-
-bool Prefetcher::enLocalRequest(u_int32_t addr) {
-  if (ifAlreadyInQueue(addr)) {
-    //printf("full\n");
-    return false;
-  }
-
-  if (ifFullLocalRequest()) {
-    return false;
-  }
-  else {
-    localRequest[rearRequest] = addr;
-    rearRequest = (rearRequest + 1) % MAX_REQUEST_COUNT;
-    return true;
-  }
-}
-
-u_int32_t Prefetcher::deLocalRequest() {
-  if (ifEmptyLocalRequest()) {
-    return 0;
-  }
-  else {
-    u_int32_t addr = localRequest[frontRequest];
-    frontRequest = (frontRequest + 1) % MAX_REQUEST_COUNT;
-    return addr;
-  }
-}
-
-u_int32_t Prefetcher::getFrontLocalRequest() {
-  return localRequest[frontRequest];
-}
-
-bool Prefetcher::ifAlreadyInQueue(u_int32_t addr) {
-  u_int32_t i;
-  for (i = frontRequest; i % MAX_REQUEST_COUNT < rearRequest; i++) {
-    if (localRequest[i % MAX_REQUEST_COUNT] / 32 == addr / 32) {
-      return true;
-    }
-  }
-  return false;
 }
