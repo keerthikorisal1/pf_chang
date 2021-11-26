3aa2ae0d67b9a28441569ae4c96b9499a55cae46
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-26 00:59:40 -0800

    Update prefetcher.C

--- prefetcher.C
+++ prefetcher.C
@@ -1,188 +1,194 @@
 #include "prefetcher.h"
 #include <stdio.h>
-#include <cmath>
+#include <climits>
 
-Prefetcher::Prefetcher() :  
-	_address_load_diff(0), _address_store_diff(0)
-{
-	_cFetch = _cReqs = 0;
+Entry::Entry(): _src(0), _dest(0), _occ(0) {}
+
+Prefetcher::Prefetcher()
+{    
+	_ready = false;
+	_oldReqCounter = 0;
+	_nextReqCounter = 0;
+    
+	_oldEntryCounter = 0;
+	_prev = 0;
+	
+	_check = false;   
 }
 
-bool Prefetcher::hasRequest(u_int32_t cycle) 
-{
-	return !_fetchQueue.empty();
+
+bool Prefetcher::mostLikelyTransition(u_int32_t from_addr, unsigned int &to_entry, float &prob) 
+{    
+	Entry e;
+	bool found = false;
+	unsigned int occ_total = 0;
+	unsigned int i;
+
+	for(i=0; i<PREFETCHER_STATE_AMOUNT; i++) 
+	{
+		if(_pairs[i]._src == from_addr/16) 
+		{
+			occ_total += _pairs[i]._occ;
+			if(_pairs[i]._occ > e._occ) 
+			{
+				e = _pairs[i];
+				to_entry = i;
+				found = true;
+			}
+		}
+	}
+	if(!found) { return false; }
+	prob *= (float)e._occ / (float)occ_total;
+	return true;
 }
 
+
+// should return true if a request is ready for this cycle
+bool Prefetcher::hasRequest(u_int32_t cycle) { return _ready || _oldReqCounter != _nextReqCounter; }
+
+// request a desired address be brought in
 Request Prefetcher::getRequest(u_int32_t cycle) 
 {
-	_cFetch = max(_cFetch, long(_fetchQueue.size()));
-
-	Request req = {0};
-
-	req.addr = _fetchQueue.front();
-
-	// Remove handling for this PC
-	_fetchQueue.pop();
-
-	return req;
+	Request r;
+	r.addr = _reqQueue[_nextReqCounter];
+	return r;
 }
 
+// this function is called whenever the last prefetcher request was successfully sent to the L2
 void Prefetcher::completeRequest(u_int32_t cycle) 
 {
-
-}
-
-int sign(long num)
-{
-	if (num >= 0) return 1;
-	return -1;
+    
+	_ready = false;
+	if(_nextReqCounter != _oldReqCounter) {
+		_nextReqCounter = (_nextReqCounter+1) % PREFETCHER_REQ_QUEUE_SIZE;
+	}
+    
 }
 
 void Prefetcher::cpuRequest(Request req) 
-{	
-	// In case of a load
-	if (req.load)
-	{	
-		// First time update diff of addresess
-		if (_address_load_diff == 0)
-			_address_load_diff = req.addr;
-
-		// If a miss / prefetch hit
-		if (!req.HitL1 || !req.fromCPU)
-		{
-			// Global history - teach and prefetch prediction
-			_globalHistoryLoads.AddMiss(req.pc, req.addr, _fetchQueue, true);
-
-			// Stride prefetcher
-			int size = _fetchQueue.size();
-			for (int i=1; i<=13-size; ++i)
-				_fetchQueue.push(req.addr + i*16);
-
-			// Delta prefetcher
-			long last_diff = ((long(req.addr) - long(_address_load_diff)) * 16) / 16;
-
-			if (last_diff > 0 && last_diff < 1024)
-				_fetchQueue.push(req.addr + last_diff);
-
-			_address_load_diff = req.addr;
-		}
-	}
-	else // In case of a store
+{
+    
+	bool found = false;
+	
+	// Modify the Markovian model according to current request.
+	for(unsigned int i=0; i<PREFETCHER_STATE_AMOUNT; i++)
 	{
-		// First time update diff of addresess
-		if (_address_store_diff == 0)
-			_address_store_diff = req.addr;
-
-		// If a miss / prefetch hit
-		if (!req.HitL1 || !req.fromCPU)
+		if(_prev == _pairs[i]._src)
 		{
-			// Global history - teach and prefetch prediction
-			_globalHistoryStores.AddMiss(req.pc, req.addr, _fetchQueue, true);
-			
-			// Stride prefetcher
-			int size = _fetchQueue.size();
-			for (int i=0; i<=13-size; ++i)
-				_fetchQueue.push(req.addr + i*16);
-		
-			// Delta prefetcher
-			long last_diff = ((long(req.addr) - long(_address_store_diff)) * 16) / 16;
-			if (last_diff > 0 && last_diff < 1024)
-				_fetchQueue.push(req.addr + last_diff);
-			
-			_address_store_diff = req.addr;
+			if(req.addr/16 == _pairs[i]._dest)
+			{
+				found = true;
+				_pairs[i]._occ+=2;
+			} 
+			else if(_pairs[i]._occ >= 1)
+			{
+				_pairs[i]._occ--;
+			}
 		}
-	}
-
-	// Limit size of queue
-	queue<u_int32_t> tmpQueue;
-	set<u_int32_t> tmpSet;
-
-	// Limit size of queue as well
-	int limit_queue = 24;
-
-	// Make sure there are no duplications
-	while (_fetchQueue.size() && limit_queue--)
-	{
-		u_int32_t curr = _fetchQueue.front();
-		_fetchQueue.pop();
-
-		if (tmpSet.count(curr) > 0)
-		{
-			continue;
-		}
-
-		tmpQueue.push(curr);
 	}
 	
-	while (tmpQueue.size())
+	unsigned int to_entry;
+	float prob = 1.0;
+	
+	// Check whether the previous prediction is correct.
+	if( req.HitL1 && _check ) 
 	{
-		_fetchQueue.push(tmpQueue.front());
-		tmpQueue.pop();
+		if( mostLikelyTransition(_checkAddr, to_entry, prob) && _checkProb*prob > THRESHOLD_PROBABILITY )
+		{
+            		if(_pairs[to_entry]._dest == req.addr/16 )
+			{
+			 	_checkAddr = req.addr;
+			 	_checkProb*=prob;
+            		}
+		}
+		else 
+		{
+			_check = false;
+		}
 	}
-
-	return;
-}
-
-short GlobalHistory::index1 = 4;
-short GlobalHistory::index2 = 1;
-
-int _main()
-{
-	queue<u_int32_t> fetchThis;
-	GlobalHistory _globalHistoryStores;
-
-	// First iteration
-	_globalHistoryStores.AddMiss(100, 0, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	_globalHistoryStores.AddMiss(200, 1, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	_globalHistoryStores.AddMiss(250, 2, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	_globalHistoryStores.AddMiss(349, 64, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	// Second iteration
-	_globalHistoryStores.AddMiss(200, 65, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	_globalHistoryStores.AddMiss(250, 66, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	_globalHistoryStores.AddMiss(349, 128, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	//system("pause");
-
-	while(fetchThis.size()) fetchThis.pop();
-	_globalHistoryStores.AddMiss(200, 129, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	/*
-	// Third iteration
-	_globalHistoryStores.AddMiss(5, 129, fetchThis, false);
-	_globalHistoryStores.PrintStacks();
-	system("pause");
-	_globalHistoryStores.AddMiss(20, 130, fetchThis, false);
-	_globalHistoryStores.PrintStacks();
-	system("pause");
-	_globalHistoryStores.AddMiss(100, 192, fetchThis, true);
-	_globalHistoryStores.PrintStacks();
-	system("pause");
-	*/
-	while (fetchThis.size())
+	else if( !req.HitL1 && _check )
 	{
-		cout << "Predicted: " << fetchThis.front() << endl;
-		fetchThis.pop();
+		if( mostLikelyTransition(_checkAddr, to_entry, prob) && _checkProb*prob > THRESHOLD_PROBABILITY )
+		{
+			if(_pairs[to_entry]._occ >= 4)
+			{
+				_pairs[to_entry]._occ-= 4;
+				_check = false;
+			}
+		}
 	}
-
-	_globalHistoryStores.PrintStacks();
-
-	return 0;
+    
+    	if(!_ready && !req.HitL1) 
+	{
+        
+		u_int32_t from_addr = req.addr;
+		float prob = 1.0;
+		unsigned int to_entry;
+		if(mostLikelyTransition(from_addr, to_entry, prob) && prob > THRESHOLD_PROBABILITY)
+		{
+			_check = true;
+			_checkAddr = from_addr;
+			_checkProb = prob;
+		}
+		
+        	if(!found)
+		{
+            
+        		_pairs[_oldEntryCounter]._src = _prev;
+    	        	_pairs[_oldEntryCounter]._dest = req.addr/16;
+    	        	_pairs[_oldEntryCounter]._occ = 1;
+   	         	_oldEntryCounter = (_oldEntryCounter + 1) % PREFETCHER_STATE_AMOUNT;
+            
+		}
+        
+		// Remove entries from the request queue if previous predictions seem incorrect.
+		// Not needed for smaller request queue because all prefetcher requests will have been issued before CPU restarts from stalling.
+		// Fill up the request queue
+		found = false;
+        
+ 		       
+	    	while(mostLikelyTransition(from_addr, to_entry, prob) && prob > THRESHOLD_PROBABILITY)
+		{
+            
+			_reqQueue[_oldReqCounter] = _pairs[to_entry]._dest * 16;
+			_oldReqCounter = (_oldReqCounter+1) % PREFETCHER_REQ_QUEUE_SIZE;
+			from_addr = _pairs[to_entry]._dest * 16;
+			found = true;
+			
+			_pairs[_oldEntryCounter] = _pairs[to_entry];
+			int i=to_entry;
+			
+			while(i!=_oldEntryCounter)
+			{
+				int j=i-1;
+				if(j<0) { j=PREFETCHER_STATE_AMOUNT-1; }
+				_pairs[i] = _pairs[j];
+				i--;
+				if(i<0) { i=PREFETCHER_STATE_AMOUNT-1; }
+			}
+			
+			_oldEntryCounter = (_oldEntryCounter + 1) % PREFETCHER_STATE_AMOUNT;
+			
+			if(_oldReqCounter == _nextReqCounter) { break; }
+		}
+		
+		if(!found) 
+		{
+			_oldReqCounter = _nextReqCounter;
+	
+			for(unsigned int i=0; i<6; i++)
+			{
+				_reqQueue[_oldReqCounter] = req.addr + 16*(i+2);
+				_oldReqCounter = (_oldReqCounter+1) % PREFETCHER_REQ_QUEUE_SIZE;
+			}
+		}
+        
+		// Get it ready to fire!
+		_ready = true;
+  	      
+		// Prepare for the next call.
+ 		_prev = req.addr/16;
+	        
+	}
+    
 }
