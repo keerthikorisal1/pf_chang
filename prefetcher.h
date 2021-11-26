69413b5233de9ac51fe146fbc89fcf7eb0499e91
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-26 00:14:46 -0800

    Update prefetcher.h

--- prefetcher.h
+++ prefetcher.h
@@ -1,45 +1,32 @@
 #ifndef PREFETCHER_H
 #define PREFETCHER_H
-#define PREFETCHER_STATE_AMOUNT 300
-#define PREFETCHER_REQ_QUEUE_SIZE 20
-#define PREFETCHER_PREV_WRITES_SIZE 10
-#define THRESHOLD_PROBABILITY 0.8
 
 #include <sys/types.h>
 #include "mem-sim.h"
 
-struct Entry {
-    Entry();
-    u_int32_t _src;
-    u_int32_t _dest;
-    unsigned int _occ;
-};
+struct Request;
 
-class Prefetcher 
-{
-  	private:
-	// states
-	bool _ready;
-	u_int32_t _reqQueue[PREFETCHER_REQ_QUEUE_SIZE];
-	unsigned int _oldReqCounter;
-	unsigned int _nextReqCounter;
+#define N 13
+#define STORE 100
 
-	Entry _pairs[PREFETCHER_STATE_AMOUNT];
-	unsigned int _oldEntryCounter;
-	u_int32_t _prev;
-	
-	bool _check;
-	u_int32_t _checkAddr;
-	float _checkProb;
+class Prefetcher {
+	private:
+	bool _readys[N];
+	bool _strider_ready;
+	Request _nextReq;
 
-	// methods
-	bool mostLikelyTransition(u_int32_t from_addr, unsigned int& to_entry, float& prob);
+	long long last_addrs[STORE];
+	short fifo;
+	long long prediction;
+	short step;
+	Request _striderReq;
 
-  	public:
+  public:
 	Prefetcher();
-
 	// should return true if a request is ready for this cycle
 	bool hasRequest(u_int32_t cycle);
+	bool hasChunkRequest();
+	bool hasStrideRequest();
 
 	// request a desired address be brought in
 	Request getRequest(u_int32_t cycle);
@@ -47,15 +34,16 @@ class Prefetcher
 	// this function is called whenever the last prefetcher request was successfully sent to the L2
 	void completeRequest(u_int32_t cycle);
 
+	// go through the last STORE addresses and determine if there are any linear strides
+	void findPattern();
+	// will get the index of last_addrs relative to fifo (addr is interpreted like fifo = 0, i.e. addr = 5 -> (fifo - 5)%STORE)
+	short getIdx(short addr);
+
 	/*
 	 * This function is called whenever the CPU references memory.
 	 * Note that only the addr, pc, load, issuedAt, and HitL1 should be considered valid data
 	 */
-	void cpuRequest(Request req);
-    
+	void cpuRequest(Request req); 
 };
-
-
-
 
 #endif
