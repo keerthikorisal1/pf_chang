0e4abe8b23688df30ed13e24f3e4190af60fa317
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-26 17:37:02 -0800

    Update prefetcher.h

--- prefetcher.h
+++ prefetcher.h
@@ -1,56 +1,60 @@
-/*
- *
- * File: prefetcher.h
- * Author: Sat Garcia (sat@cs)
- * Description: Header file for prefetcher implementation
- *
- */
-
 #ifndef PREFETCHER_H
 #define PREFETCHER_H
 
 #include <sys/types.h>
 #include "mem-sim.h"
 
+#define L2_BLOCK 32
+#define L2_BLOCK 16
+
+#define MAX_REQUESTS 512
+#define NUM_STREAMS 8
+
 #define NUM_RPT_ENTRIES 128 
-#define NUM_MAX_REQUESTS 40 
 #define REQUEST_CUTOFF 20
 #define DEFAULT_STREAMSIZE 2
 
-struct RPT {
-    u_int32_t pc;
-    u_int32_t prev_addr;
-    u_int32_t stride;
-    int32_t state;
-} ;// reference prediction table.
+struct SLH {
+	u_int32_t pc;	     //PC of stream
+	u_int32_t addr;      //Last Address Accessed
+	u_int16_t length;    //Length of stream
+	u_int32_t lifetime;  //Lifetime of stream
+};
 
+struct RPT {
+  u_int32_t pc;
+  u_int32_t prev_addr;
+  u_int32_t stride;
+  int32_t state;
+};
 
 class Prefetcher {
-    private:
-        int num_requests;
-        int num_rpt;
-        Request requests[NUM_MAX_REQUESTS];
-        RPT rpt[NUM_RPT_ENTRIES];
-        int current_pending_request;
-        int oldest_rpt;
-    public:
-        Prefetcher();
-        // should return true if a request is ready for this cycle
-        bool hasRequest(u_int32_t cycle);
+  private:
+      bool _ready;
+      Request _nextReq;
+      SLH SLH_TABLE[STREAM_COUNT];
 
-        // request a desired address be brought in
-        Request getRequest(u_int32_t cycle);
+      u_int16_t live_Streams;
+      u_int16_t stream_buff[STREAM_COUNT];
+      u_int32_t requestQueue[MAX_REQUESTS];
+      u_int16_t num_req;
 
-        // this function is called whenever the last prefetcher request was successfully sent to the L2
-        void completeRequest(u_int32_t cycle);
+      void streamReset(u_int16_t index);
+      u_int16_t inStream(u_int32_t addr);
+      void addRequest(u_int32_t addr);
 
-        /*
-         * This function is called whenever the CPU references memory.
-         * Note that only the addr, pc, load, issuedAt, and HitL1 should be considered valid data
-         */
-        void cpuRequest(Request req);
-        void addRequest(u_int32_t addr);
-        bool ifAlreadyInQueue(u_int32_t addr);
+      int num_rpt;
+      int oldest_rpt;
+      RPT rpt[NUM_RPT_ENTRIES];
+  
+  public:
+      Prefetcher();
+
+      bool hasRequest(u_int32_t cycle);
+      Request getRequest(u_int32_t cycle);
+      void completeRequest(u_int32_t cycle);
+      void cpuRequest(Request req); 
+
 };
 
 #endif
