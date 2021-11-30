eff5195d3c87bb7e435b4f29a1ec1961b0e7fec4
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-29 21:40:43 -0800

    Update prefetcher.C

--- prefetcher.C
+++ prefetcher.C
@@ -6,7 +6,6 @@
 using namespace std;
 
 static char tags[STATE_SIZE];
-/*static char rpt_check[NUM_RPT_ENTRIES/8 +1]; */
 static rpt_row_entries rpt_table[NUM_RPT_ENTRIES];
 
 Prefetcher::Prefetcher(){
@@ -16,7 +15,7 @@ Prefetcher::Prefetcher(){
     for(i = 0; i < NUM_RPT_ENTRIES; i++){
         rpt_table[i].pc = 0;
         rpt_table[i].stride = 0;
-        rpt_table[i].last_mem_access = 0;
+        rpt_table[i].prev_addr = 0;
     }
 }
 
@@ -26,18 +25,18 @@ Request Prefetcher::getRequest(u_int32_t cycle) { return _nextReq; }
 
 void Prefetcher::completeRequest(u_int32_t cycle) {
     int rpt_row, current_stride;
-    rpt_row_entries *current_row;
+    rpt_row_entries *current_rtp_row;
 
     if(_req_left == 0){
         _ready = false;
     }
     else{
-        std::cout << "called cpmplete req && in RPT"
         _req_left--;
         rpt_row = _nextReq.pc % NUM_RPT_ENTRIES;
-        current_row = &rpt_table[rpt_row];
-        if(current_row->pc == _nextReq.pc){
-            _nextReq.addr = _nextReq.addr + current_row->stride;
+        current_rpt_row = &rpt_table[rpt_row];
+        if(current_rpt_row->pc == _nextReq.pc){
+            std::cout << "called cpmplete req && in RPT"
+            _nextReq.addr = _nextReq.addr + current_rpt_row->stride;
         }
         else{
             std::cout << "called cpmplete req && not in RPT"
@@ -48,15 +47,15 @@ void Prefetcher::completeRequest(u_int32_t cycle) {
 
 void Prefetcher::cpuRequest(Request req){
     int rpt_row, current_stride;
-    rpt_row_entries *current_row;
+    rpt_row_entries *current_rpt_row;
 
     if(req.HitL1 && !_ready){
         rpt_row = req.pc % NUM_RPT_ENTRIES;
-        current_row = &rpt_table[rpt_row];
-        if(current_row->pc == req.pc){
+        current_rpt_row = &rpt_table[rpt_row];
+        if(current_rpt_row->pc == req.pc){
             /*std::cout << "current PC: %s :" << req.pc << "\n";
             printStruct(current_row);*/
-            current_stride = current_row->stride;
+            current_stride = current_rpt_row->stride;
             _nextReq.addr = req.addr + current_stride;
         }
         else{
@@ -67,15 +66,15 @@ void Prefetcher::cpuRequest(Request req){
     }
     else if (!req.HitL1) {
         rpt_row = req.pc % NUM_RPT_ENTRIES;
-        current_row = &rpt_table[rpt_row];
-        if(current_row->pc == req.pc){
-            if((current_stride = req.addr - (current_row->last_mem_access)) == current_row->stride){
+        current_rpt_row = &rpt_table[rpt_row];
+        if(current_rpt_row->pc == req.pc){
+            if((current_stride = req.addr - (current_rpt_row->prev_addr)) == current_rpt_row->stride){
                 /*std::cout << "current stride: %s\n" << current_stride << "\n";
                 printStruct(current_row);*/
                 _nextReq.addr = req.addr + current_stride;
             }
             else{
-                current_row->stride = current_stride;
+                current_rpt_row->stride = current_stride;
                 /*std::cout << "current stride: %s\n" << current_stride << "\n";
                 printStruct(*current_row);*/
                 _nextReq.addr = req.addr + L2_BLOCK;
@@ -83,10 +82,10 @@ void Prefetcher::cpuRequest(Request req){
         }
         else{
             _nextReq.addr = req.addr + L2_BLOCK;
-            current_row->stride = 0;
+            current_rpt_row->stride = 0;
         }
-        current_row->pc = req.pc;
-        current_row->last_mem_access = req.addr;
+        current_rpt_row->pc = req.pc;
+        current_rpt_row->prev_addr = req.addr;
         /*std::cout << "last_addr: %s\n" << req.addr << "\n";
         std::cout << "current PC: %s\n" << req.pc << "\n";
         printStruct(current_row);*/
@@ -98,6 +97,6 @@ void Prefetcher::cpuRequest(Request req){
 
 void Prefetcher::printStruct(rpt_row_entries *current_row){
     std::cout << "PC: \n" << current_row->pc << "\n";
-    std::cout << "Prev_Add: \n" << current_row->last_mem_access << "\n";
+    std::cout << "Prev_Add: \n" << current_row->prev_addr_access << "\n";
     std::cout << "Current Stride: \n" << current_row->stride << "\n";
 }
