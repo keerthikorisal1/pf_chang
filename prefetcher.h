1b11f8d3efd88becfdb90d9179bd3a62f015f407
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-29 19:24:06 -0800

    Update prefetcher.h

--- prefetcher.h
+++ prefetcher.h
@@ -3,6 +3,8 @@
 
 #include <sys/types.h>
 #include "mem-sim.h"
+#include <cstdlib>
+#include <cstring>
 
 #define L2_BLOCK 32
 #define L1_BLOCK 16
@@ -11,42 +13,21 @@
 #define STREAM_COUNT 8
 
 #define NUM_RPT_ENTRIES 128 
-#define REQUEST_CUTOFF 20
-#define DEFAULT_STREAMSIZE 2
+#define STATE_SIZE 4096
+#define NUM_REQ_PERMISS 3
 
-struct SLH {
-	u_int32_t pc;	     //PC of stream
-	u_int32_t addr;      //Last Address Accessed
-	u_int16_t length;    //Length of stream
-	u_int32_t lifetime;  //Lifetime of stream
-};
 
 struct RPT {
   u_int32_t pc;
-  u_int32_t prev_addr;
+  u_int32_t last_mem_access;
   u_int32_t stride;
-  int32_t state;
 };
 
 class Prefetcher {
   private:
       bool _ready;
-      Request requests[MAX_REQUESTS];
-      SLH SLH_TABLE[STREAM_COUNT];
-
-      u_int16_t live_streams;
-      u_int16_t stream_buff[STREAM_COUNT];
-      u_int32_t requestQueue[MAX_REQUESTS];
-      u_int16_t num_req;
-
-      void streamReset(u_int16_t index);
-      u_int16_t inStream(u_int32_t addr);
-      void addRequest(u_int32_t addr);
-
-      int num_rpt;
-      int oldest_rpt;
-      int current_pending_request;
-      RPT rpt[NUM_RPT_ENTRIES];
+      Request _nextReq;
+      int _req_left;
   
   public:
       Prefetcher();
@@ -55,7 +36,6 @@ class Prefetcher {
       Request getRequest(u_int32_t cycle);
       void completeRequest(u_int32_t cycle);
       void cpuRequest(Request req);
-      bool ifAlreadyInQueue(u_int32_t addr); 
 
 };
 
