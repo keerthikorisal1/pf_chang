076b5749d1936654bab7f410fc2c5a9ce26a460c
keerthikorisal1 <76765212+keerthikorisal1@users.noreply.github.com>
2021-11-29 20:53:25 -0800

    Update prefetcher.C

--- prefetcher.C
+++ prefetcher.C
@@ -49,7 +49,7 @@ void Prefetcher::cpuRequest(Request req){
         rpt_row = req.pc % NUM_RPT_ENTRIES;
         current_row = &rpt_table[rpt_row];
         if(current_row->pc == req.pc){
-            printf("current PC: %s\n", req.pc);
+            std::cout << "current PC: %s\n", req.pc);
             /*printStruct(current_row);*/
             current_stride = current_row->stride;
             _nextReq.addr = req.addr + current_stride;
@@ -65,16 +65,16 @@ void Prefetcher::cpuRequest(Request req){
         current_row = &rpt_table[rpt_row];
         if(current_row->pc == req.pc){
             if((current_stride = req.addr - (current_row->last_mem_access)) == current_row->stride){
-                printf("current stride: %s\n", current_stride);
-                printf("current rpt stride: %s\n", current_row->stride);
+                /*printf("current stride: %s\n", current_stride);
+                printf("current rpt stride: %s\n", current_row->stride);*/
                 _nextReq.addr = req.addr + current_stride;
             }
             else{
                 current_row->stride = current_stride;
-                printf("current stride: %s\n", current_stride);
+                /*printf("current stride: %s\n", current_stride);
                 printf("current rpt stride: %s\n", current_row->stride)
                 printf("PC: %s\n", current_rpt->pc)
-                printf("Prev_Add: %s\n", current_rpt->last_mem_access);
+                printf("Prev_Add: %s\n", current_rpt->last_mem_access);*/
                 _nextReq.addr = req.addr + L2_BLOCK;
             }
         }
@@ -84,7 +84,7 @@ void Prefetcher::cpuRequest(Request req){
         }
         current_row->pc = req.pc;
         current_row->last_mem_access = req.addr;
-        printf("last_addr: %s\n", req.addr);
+        /*printf("last_addr: %s\n", req.addr);
         printf("current PC: %s\n", req.pc);
         /*printStruct(current_row);*/
         _ready = true;
